78bd379a0a1e22f644bebf1acb806af03e572d6f
Tiago Persch <thiagopersch@gmail.com>
2023-08-20 11:16:23 -0400

    Added new modifications in page login

--- frontend/src/templates/Login/index.tsx
+++ frontend/src/templates/Login/index.tsx
@@ -1,10 +1,33 @@
 import Heading from "@/components/Heading";
 import Base from "../Base";
+import SectionContainer from "@/components/SectionContainer";
+import TextInput from "@/components/TextInput";
+
+import * as S from "./styles";
+import Button from "@/components/Button";
+import { LogIn } from "@styled-icons/feather";
 
 const Login = () => {
   return (
     <Base>
-      <Heading>Login</Heading>
+      <SectionContainer>
+        <S.Wrapper>
+          <Heading size="md">Login</Heading>
+          <S.WrapperInputs>
+            <S.Input>
+              <TextInput name="email" label="E-mail" />
+            </S.Input>
+            <S.Input>
+              <TextInput name="password" label="Senha" />
+            </S.Input>
+          </S.WrapperInputs>
+          <S.CTA>
+            <Button size="large" icon={<LogIn />}>
+              Entrar
+            </Button>
+          </S.CTA>
+        </S.Wrapper>
+      </SectionContainer>
     </Base>
   );
 };
---
+++ frontend/src/templates/Login/styles.ts
@@ -0,0 +1,45 @@
+import styled, { css } from "styled-components";
+import media from "styled-media-query";
+
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    background: #e9e9e9e9;
+    padding: ${theme.spacings.medium};
+    border-radius: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    box-shadow: ${theme.shadow.default};
+  `}
+`;
+
+export const WrapperInputs = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+  `}
+`;
+
+export const Input = styled.div`
+  width: 50%;
+
+  ${media.lessThan("medium")`
+    width: 100%;
+  `}
+`;
+
+export const CTA = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: center;
+    align-items: stretch;
+
+    ${media.lessThan("medium")`
+        flex-direction: column-reverse;
+        width: 100%;
+    `}
+  `}
+`;
